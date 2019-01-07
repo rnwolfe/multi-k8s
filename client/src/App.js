7a78c201bc780beb87e3406d167c5366b824c611
Ryan Wolfe <rn.wolfe@gmail.com>
2019-01-07 12:58:52 -0500

    add footer to client

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,37 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
+
+const footerStyle = {
+  backgroundColor: "#282c34",
+  fontSize: "14px",
+  color: "white",
+  borderTop: "1px solid #282c34",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
+const phantomStyle = {
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "100%"
+};
+
+function Footer({ children }) {
+  return (
+    <div>
+      <div style={phantomStyle} />
+      <div style={footerStyle}>{children}</div>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
@@ -18,6 +49,11 @@ class App extends Component {
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
           </div>
+          <Footer>
+            <span>There should be no downtime given AWS EBS rolling updates.</span>
+            <br /><br />
+            <span>This is a test app.</span>
+          </Footer>
         </div>
       </Router>
     );
